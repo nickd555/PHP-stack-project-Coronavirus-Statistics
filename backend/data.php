813cf2c99706e1d30ea2d9066f4c7dc58f358e52
Nick <nickd555@outlook.com>
2020-03-27 16:54:14 +0100

    Initial commit

---
+++ backend/data.php
@@ -0,0 +1,63 @@
+<?php
+
+class dataHandler
+{
+	function insertData($date, $infections, $fatalities) {
+		$root = $_SERVER['DOCUMENT_ROOT'];
+		$db = new SQLite3($root . '/backend/CoronaData.db');
+		$fatalityRatio = $fatalities/$infections*100;
+		$sql = "INSERT INTO CoronaData(Date, Infections, Fatalities, FatalityRatio) VALUES('$date', '$infections', '$fatalities', '$fatalityRatio')";
+
+		try {
+			$db->exec($sql);
+			echo 'The data has been succesfully inserted!';
+		} catch (Exception $e) {
+			echo 'There was a problem inserting the data, please check your formatting. Caught exception: ', $e->getMessage(), '\n';
+		}
+	}
+
+	function removeData($entry) {
+		$root = $_SERVER['DOCUMENT_ROOT'];
+		$db = new SQLite3($root . '/backend/CoronaData.db');
+		$sql = "DELETE FROM CoronaData WHERE entry = '$entry'";
+
+		try {
+			$db->exec($sql);
+			echo 'The data has been succesfully removed!';
+		} catch (Exception $e) {
+			echo 'There was a problem removing the data, please check your formatting. Caught exception: ', $e->getMessage(), '\n';
+		}
+	}
+
+	function collectData() {
+		$root = $_SERVER['DOCUMENT_ROOT'];
+		$db = new SQLite3($root . '/backend/CoronaData.db');
+		$result = $db->query('SELECT * FROM CoronaData');
+		$row = array();
+		
+		while ($res = $result->fetchArray()){
+			array_push($row, $res);
+		}
+
+		echo '<table border="1">';
+		echo '<th>Entry</th>
+		<th>Date</th> 
+		<th>Infections</th>
+		<th>Fatalities</th>
+		<th>FatalityRatio</th>'; 
+
+		foreach($row as $entry) {
+			echo '<tr>' . 
+			'<td>' . $entry['Entry'] . '</td>' .
+			'<td>' . $entry['Date'] . '</td>' . 
+			'<td>' . $entry['Infections'] . '</td>' . 
+			'<td>' . $entry['Fatalities'] . '</td>' . 
+			'<td>' . ROUND($entry['FatalityRatio'], 2) . '%' . '</td>' .
+			'</tr>';
+		}
+
+		echo '</table>';
+
+	}
+}
+?>
---
+++ index.php
@@ -0,0 +1,49 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <?php include 'backend/data.php'; ?>
+  <head>
+    <?php $dataHandler = new dataHandler; ?>
+  </head>
+  <body>
+  	<div>
+  		<h1>Test Application</h1>
+  	</div>
+  	<div>
+  		<p>
+  			Hello! This is my test application. Please use the tools below to insert or remove data.
+  		</p>
+  	</div>
+    <div>
+      <form method="post">
+        <h3> Insert Data </h3>
+        <p> Use this section to insert new data. Dates follow the pattern of Year-Month-Days Hour:Minutes:Seconds, Infections and Fatalities accept any number.</p>
+        Date (YYYY-mm-dd hh:ii:ss): <input type="text" name="Date"> <br>
+        Number of infections: <input type="number" name="Infections"> <br>
+        Number of Fatalities: <input type="number" name="Fatalities"><br>
+        <input type="submit" value="add" name="submit">
+      </form>
+    </div>
+    <div>
+        <form method="post">
+        <h3> Remove Data </h3>
+        <p> If you wish to delete any data from the table, enter the Entry number in the box below and hit submit</p>
+        Entry number: <input type="number" name="Entry"> <br>
+        <input type="submit" value="remove" name="submit">
+
+      </form>
+    </div>
+      <?php 
+        switch($_POST['submit']) {
+          case 'add':
+          $dataHandler->insertData($_POST['Date'], (int)$_POST['Infections'], (int)$_POST['Fatalities']);
+          break;
+          case 'remove':
+          $dataHandler->removeData((int)$_POST['Entry']);
+          break;
+        }
+      ?>
+  	<div>
+      <h3> Current Data </h3>
+  		<?php $dataHandler->collectData(); ?>
+  	</div>
+  </body>
+</html>
